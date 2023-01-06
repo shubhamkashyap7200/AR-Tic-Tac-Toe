9149191d9ec50c13d8f96353d4ff4f48af2bcfd2
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-01-06 22:23:19 +0530

    ARView added

--- XOXO/ViewController.swift
+++ XOXO/ViewController.swift
@@ -4,61 +4,78 @@ import ARKit
 import MultipeerConnectivity
 
 class ViewController: UIViewController, ARSessionDelegate {
-  
-  // MARK: - Properties
-  
-  // MARK: - IBOutlets & IBActions
-  
-  @IBOutlet var arView: ARView!
-  @IBOutlet weak var message: UILabel!
-  
-  @IBAction func player1ButtonPressed(_ sender: Any) {
-  }
-  
-  @IBAction func player2ButtonPressed(_ sender: Any) {
-  }
-  
-  @IBAction func clearButtonPressed(_ sender: Any) {
-  }
-  
-  // MARK: - AR View Functions
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
+    
+    // MARK: - Properties
+    var playerColor: UIColor = .blue
+    
+    // MARK: - IBOutlets & IBActions
+    
+    @IBOutlet var arView: ARView!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func player1ButtonPressed(_ sender: Any) {
+        playerColor = .blue
+    }
+    
+    @IBAction func player2ButtonPressed(_ sender: Any) {
+        playerColor = .red
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+    }
+    
+    // MARK: - AR View Functions
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func initARView() {
+        arView.session.delegate = self
+        arView.automaticallyConfigureSession = false
+        
+        let arConfiguration = ARWorldTrackingConfiguration()
+        arConfiguration.planeDetection = [ .horizontal ]
+        arConfiguration.environmentTexturing = .automatic
+        arView.session.run(arConfiguration)
+    }
 }
 
 // MARK: - Model Entity Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    // Add code here...
+    
 }
 
 // MARK: - Gesture Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    // Add code here...
+    
 }
 
 
 // MARK: - Multipeer Session Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    // Add code here...
+    
 }
 
 // MARK: - Helper Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    // Add code here...
+    func sendMessage(_ message: String) {
+        DispatchQueue.main.async {
+            self.message.text = message
+        }
+    }
 }
 
 
